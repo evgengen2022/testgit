c0a2690144696dc878c4f7694ec5f9717a637656
Alex Skorohod <alexey.skorohod@gmail.com>
2022-02-20 21:39:01 +0200

    Prettier & ESLint configs

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hi PC')
-})
+	res.send('Hi PC');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+	console.log(`Example app listening on port ${port}`);
+});
